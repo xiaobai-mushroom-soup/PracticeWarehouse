80a45291803956b734258e51de57b663a49e32d3
richGuo <rguo9659@gmail.com>
2023-06-29 00:41:49 +0800

    rust:ownership

---
+++ rust/ownership/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // let s = "hello";
+    // println!("{s}");
+
+    // let mut s = String::from("这是一段用String声明的字符串");
+
+    // println!("{s}");
+    // s.push_str(",hahahah");
+    // println!("{s}");
+    let s1 = String::from("test");
+   let s2 =  test(s1);
+    println!("{}sss",s2);
+    
+}
+fn test (str:String)-> String{
+     str
+}
---
+++ rust/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/richguo/Desktop/study/PracticeWarehouse/rust/ownership/target/debug/ownership: /Users/richguo/Desktop/study/PracticeWarehouse/rust/ownership/src/main.rs
